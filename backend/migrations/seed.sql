a59739e3341a2182b84ddc33c2484f3e0d77c6c0
rawanalamirah <rawanalamirah@hotmail.com>
2025-08-30 00:24:16 +0300

    final commit hopefully

--- backend/migrations/seed.sql
+++ backend/migrations/seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO loans (application_number, applicant_name, loan_amount, status) VALUES
-    (1001, 'Munira Amer', 10000, 'Pending'),
-    (1002, 'Nawaf Nasser', 11500, 'Approved'),
+    (1001, 'Rawan Nail', 40000, 'Pending'),
+    (1002, 'Nawaf Yousef', 11500, 'Approved'),
     (1003, 'Nora Ahmad', 8000, 'Rejected')
 ON CONFLICT DO NOTHING;
